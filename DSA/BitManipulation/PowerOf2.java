cfabf9ee18843f83add4c9002e0c0ff16a1c7728
Swetharasi Lakshmi Narasimha <lakshminarasimha021215@gmail.com>
2025-12-15 12:41:00 +0530

    PowerOf2 (12-12-2025)

---
+++ DSA/BitManipulation/PowerOf2.java
@@ -0,0 +1,23 @@
+package dsa.bitmanipulation;
+
+import java.util.Scanner;
+
+public class PowerOf2 {
+
+	public static boolean powerOf2(int n) {
+		if(n==0) {
+			return false;
+		}
+		return (n & (n-1))==0;
+	}
+	public static void main(String[] args) {
+		Scanner sc = new Scanner (System.in);
+	    int n = sc.nextInt();
+	    
+	    System.out.println(powerOf2(n));
+	    
+	    sc.close();
+
+	}
+
+}
