cc6e03ea36fb6e447f5f6bc1d37bbdcb87500148
Swetharasi Lakshmi Narasimha <lakshminarasimha021215@gmail.com>
2025-12-15 13:01:35 +0530

    CountSetBits (13-12-2025)

---
+++ DSA/BitManipulation/CountSetBits.java
@@ -0,0 +1,27 @@
+package dsa.bitmanipulation;
+
+import java.util.Scanner;
+
+public class CountSetBits {
+
+	public static int countSetBits(int n) {
+		int count = 0;
+		while(n>0) {
+			n = n & n-1;
+			count++;
+		}
+		return count;
+	}
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		
+		int res = countSetBits(n);
+		
+		System.out.println(res);
+		
+		sc.close();
+
+	}
+
+}
